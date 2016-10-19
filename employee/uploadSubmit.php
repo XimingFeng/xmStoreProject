2840eac245ccbe6229f27c9dc2f68a66b5bf0876
XimingFeng <xfeng36@asu.edu>
2016-10-19 14:49:41 -0700

    typo

--- employee/uploadSubmit.php
+++ employee/uploadSubmit.php
@@ -1,5 +1,5 @@
 <?php
-	echo "here we go, is is uploadSubmit.php"
+	echo "here we go, is is uploadSubmit.php";
 	$uploadDir = "/upload/";
 	$targetFile = $uploadDir. basename($_FILES['picToUpload']['name']);
 	$uploadOK = 1;
