a5b5d3df292b4d6243833085f3ac5e385c2da61e
XimingFeng <xfeng36@asu.edu>
2016-10-19 15:35:42 -0700

    change the upload dir

--- employee/uploadSubmit.php
+++ employee/uploadSubmit.php
@@ -1,6 +1,6 @@
 <?php
-	echo "here we go, is is uploadSubmit.php";
-	$uploadDir = "/upload/";
+	echo "here we go, this is uploadSubmit.php";
+	$uploadDir = "upload/";
 	$targetFile = $uploadDir. basename($_FILES['picToUpload']['name']);
 	$uploadOK = 1;
 	$imageFileType = pathinfo($targetFile,PATHINFO_EXTENSION) ;
@@ -19,18 +19,23 @@
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 	}
+
 	// Check file size
 	if ($_FILES["picToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
-	}if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	}
+
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 		&& $imageFileType != "gif" ) {
     	echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     	$uploadOk = 0;
 	}
+
 	if($uploadOk == 0){
 		echo "Sorry, your file was not uploaded.";
-	}else{
+	}
+	else{
 		if(move_uploaded_file($_FILES['picToUpload']["temp_name"], $targetFile)){
 			echo "The file ". basename( $_FILES["picToUpload"]["name"]). " has been uploaded.";
 		}
