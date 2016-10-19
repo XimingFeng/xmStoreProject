3c8ea79d919b21851f1c45c8af66b8ed13c41d5a
XimingFeng <xfeng36@asu.edu>
2016-10-18 23:46:26 -0700

    change post direction to register.php

--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 	include('signUpSession.php'); 
 	if(isset($_SESSION['newUser'])){
 		// The header() function sends a raw HTTP header to a client.
+		$_SESSION['login_user'] = $_SESSION['newUser'];
 		header("Location: http://".$_SERVER['HTTP_HOST']."/employee/employeeHomePage.php");
 	}
 ?>
@@ -25,7 +26,7 @@
 		<div class="row">
 				<div class="col-md-6 col-md-offset-3">
 						<h1>Sign Up!</h1>
-						<form action=<?php echo "\"http://". $_SERVER['HTTP_HOST'] . "\"";?> method="post" 
+						<form action=<?php echo "\"http://". $_SERVER['HTTP_HOST'] . "/register,php\"";?> method="post" 
 							class="form-vertical">
 							<div class="form-group">
 								<label for="email">Email address:</label>
