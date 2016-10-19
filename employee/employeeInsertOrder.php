bce5c525dcbc7db4d8f323477b70d0b61cb5f891
XimingFeng <xfeng36@asu.edu>
2016-10-19 14:10:49 -0700

    add uploading pic function

--- employee/employeeInsertOrder.php
+++ employee/employeeInsertOrder.php
@@ -4,18 +4,25 @@
 	<title></title>
 </head>
 <body>
-	<form action = <?php  echo "\" http://". $_SERVER['HTTP_HOST']."/employee/employeeInsertOrder.php"?> >
+	<form action = <?php  echo "\" http://". $_SERVER['HTTP_HOST']."/employee/employeeInsertOrder.php"?> method = "post" enctype = "multipart/form-data">
 		brandID: <input type="text" name = "brandID">
 		</input><br>
+
 		productEnglishName: <input type="text" name = "productEnglishName">
 		</input><br>
+
 		productChineseName: <input type="text" name = "productChineseName">
 		</input><br>
+
 		PRCPrice: <input type="text" name = "PRCPrice">
 		</input><br>
+
 		USAPrice: <input type="text" name = "USAPrice">
 		</input><br>
-		URL: <input type="text" name = "URL"><br>
+
+		<input type="file" name="productPic" > </input><br>
+
+		URL: <input type="text" name = "URL"> </input><br>
 		<input type="submit"></input>
 		</input>
 	</form>
