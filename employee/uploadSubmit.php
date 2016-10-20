8f75cf36f65997a213bdb88039e03dc10823b59b
XimingFeng <xfeng36@asu.edu>
2016-10-19 18:23:24 -0700

    show argument of move uploaded

--- employee/uploadSubmit.php
+++ employee/uploadSubmit.php
@@ -48,8 +48,8 @@
 		echo "Sorry, your file was not uploaded.";
 	}
 	else{
-		echo "     $_FILES['picToUpload']['tmp_name'] is first argument    ";
-		echo '$targetFile is the second argument    ';
+		echo $_FILES['picToUpload']['tmp_name']. " is the first argument";
+		echo $targetFile. " is the second argument";
 		if(move_uploaded_file($_FILES['picToUpload']['tmp_name'], "$targetFile")){
 			echo "The file ". basename( $_FILES["picToUpload"]["name"]). " has been uploaded.";
 		}
