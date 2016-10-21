ecbae69769282caa38f395164d3693dfa53862d8
XimingFeng <xfeng36@asu.edu>
2016-10-20 23:29:02 -0700

    debug can not connect s3

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -1,9 +1,9 @@
 <?php
 	use Aws\S3\S3Client;
 	require 'vendor/autoload.php';
-	// echo $_SERVER['HTTP_HOST'] . '/vendor/autoload.php';
+	
 	echo 'If the file exits:   ' . file_exists('/vendor/autoload.php');
-	// $config = require('uploadConfig.php');
+	$config = require('uploadConfig.php');
 	// $s3 = S3Client::factory([
 		// 'key' = $config['s3']['key'];
 		// 'secret' = $config['s3']['key'];
