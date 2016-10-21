e52413dee6ba6fcb7f33856d6aa3698937d77c64
XimingFeng <xfeng36@asu.edu>
2016-10-20 23:59:42 -0700

    delete config for security

--- s3/uploadConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-	return [
-		's3' => [
-			'key' => 'AKIAJQY4DLRNONGNBJKA',
-			'secret' => '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',
-			'bucket' => 'elasticbeanstalk-us-west-2-772115187324'
-		]
-	];
-?>
