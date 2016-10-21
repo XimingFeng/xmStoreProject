8a5eedc269c86ad0d35090909807189432b9ff34
XimingFeng <xfeng36@asu.edu>
2016-10-20 23:35:16 -0700

    check if it can find vendor

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -2,7 +2,8 @@
 	use Aws\S3\S3Client;
 	require 'vendor/autoload.php';
 	
-	echo 'If the file exits:   ' . file_exists('vendor/autoload.php');
+	echo 'If the file exits:   ' . file_exists('vendor');
+	
 	$config = require('uploadConfig.php');
 	$s3 = S3Client::factory([
 		'key' => $config['s3']['key'];
