5912c6f0c41dec081a71754714c02b54835d602b
XimingFeng <xfeng36@asu.edu>
2016-10-20 23:33:51 -0700

    typooooooo

--- s3/uploadConfig.php
+++ s3/uploadConfig.php
@@ -1,6 +1,6 @@
 <?php
 	return [
-		s3 => [
+		's3' => [
 			'key' => 'AKIAJQY4DLRNONGNBJKA',
 			'secret' => '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',
 			'bucket' => 'elasticbeanstalk-us-west-2-772115187324'
