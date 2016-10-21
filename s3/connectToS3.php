5fae7652d5d316999a33a6745c2e7b3c60a87253
XimingFeng <xfeng36@asu.edu>
2016-10-20 23:27:25 -0700

    to see if autoload.php exist

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -2,7 +2,7 @@
 	use Aws\S3\S3Client;
 	require 'vendor/autoload.php';
 	// echo $_SERVER['HTTP_HOST'] . '/vendor/autoload.php';
-	echo 'If the file exits:   ' . file_exists($_SERVER['HTTP_HOST'] . '/vendor/autoload.php');
+	echo 'If the file exits:   ' . file_exists('/vendor/autoload.php');
 	// $config = require('uploadConfig.php');
 	// $s3 = S3Client::factory([
 		// 'key' = $config['s3']['key'];
