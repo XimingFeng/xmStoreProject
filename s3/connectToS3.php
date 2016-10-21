3a69745eedd18bf17ec38202eef1eed7365f0407
XimingFeng <xfeng36@asu.edu>
2016-10-20 23:30:07 -0700

    same as previous

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -4,8 +4,8 @@
 	
 	echo 'If the file exits:   ' . file_exists('/vendor/autoload.php');
 	$config = require('uploadConfig.php');
-	// $s3 = S3Client::factory([
-		// 'key' = $config['s3']['key'];
-		// 'secret' = $config['s3']['key'];
-	// ]);
+	$s3 = S3Client::factory([
+		'key' = $config['s3']['key'];
+		'secret' = $config['s3']['key'];
+	]);
 ?>
