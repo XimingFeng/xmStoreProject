0da16bb01cd39c633e39e9b36bbbf26b1e90f119
XimingFeng <xfeng36@asu.edu>
2016-10-20 19:43:01 -0700

    remove echo

--- login.php
+++ login.php
@@ -6,33 +6,33 @@
 	$databasePort = 3306;
 	$databasePasswords = "ximing1993";
 	$targetDatabase = "xmStore";
-	echo "login page start";
-	echo $_SERVER['HTTP_HOST'];
+	// echo "login page start";
+	// echo $_SERVER['HTTP_HOST'];
 
 	if(isset($_POST["submit"])){
-		echo "submit is set";
+		// echo "submit is set";
 		if(empty($_POST["userName"]) || empty($_POST["password"])){
-			echo "inValid User Name";
+			// echo "inValid User Name";
 			// $error = "User name or password is invalid";
 		}
 	}
 	else{
 		$userName = $_POST["userName"];
 		$password = $_POST["password"];
-		echo "<br/>";
-		echo "The submission is not empty";
-		echo "The user name is ".$userName."<br/>";
+		// echo "<br/>";
+		// echo "The submission is not empty";
+		// echo "The user name is ".$userName."<br/>";
 		
 		//create an dataBase connection
 		$connection = mysqli_connect($databaseDNS, $databaseUser, $databasePasswords, $targetDatabase, $databasePort);
-		echo "connect finished";
+		// echo "connect finished";
 
 		if (mysqli_connect_errno())
   		{
   			echo "Failed to connect to MySQL: " . mysqli_connect_error();
   		}
   		else{
-  			echo "connet succesed!!!";
+  			// echo "connet succesed!!!";
   			$db = mysqli_select_db($connection, "xmStore");
   			$sql = 
 			"
@@ -44,9 +44,9 @@
 			$query = $connection->query($sql);
 			$row = $query->num_rows;
 			if($row == 1){
-				echo "Congrat! We found ya!";
+				// echo "Congrat! We found ya!";
 				$_SESSION['login_user'] = $_POST["userName"];
-				echo "session Login user is ".$_SESSION['login_user'];
+				// echo "session Login user is ".$_SESSION['login_user'];
 				echo $_SERVER['HTTP_HOST']."/employee/employeeHomePage.php";
 				header("Location: http://".$_SERVER['HTTP_HOST']."/employee/employeeHomePage.php");
 			}else{
