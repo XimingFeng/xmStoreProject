d89ddea1ae3498a3ce687318a4a2b923eae9fa4d
XimingFeng <xfeng36@asu.edu>
2016-10-20 23:19:44 -0700

    same as previous

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -1,6 +1,6 @@
 <?php
 	use Aws\S3\S3Client;
-	// require $_SERVER['HTTP_HOST'] . '/vendor/autoload.php';
+	require ($_SERVER['HTTP_HOST'] . '/vendor/autoload.php');
 	echo $_SERVER['HTTP_HOST'] . '/vendor/autoload.php';
 	echo file_exists($_SERVER['HTTP_HOST'] . '/vendor/autoload.php');
 	// $config = require('uploadConfig.php');
