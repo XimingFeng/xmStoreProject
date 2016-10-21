ce739acdb44cc003ece009140cfefca799a221e6
XimingFeng <xfeng36@asu.edu>
2016-10-20 23:14:19 -0700

    same to previous

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -2,7 +2,7 @@
 	use Aws\S3\S3Client;
 	// require $_SERVER['HTTP_HOST'] . '/vendor/autoload.php';
 	echo $_SERVER['HTTP_HOST'] . '/vendor/autoload.php';
-	$config = require('uploadConfig.php');
+	// $config = require('uploadConfig.php');
 	$s3 = S3Client::factory([
 		'key' = $config['s3']['key'];
 		'secret' = $config['s3']['key'];
