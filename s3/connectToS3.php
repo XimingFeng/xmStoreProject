3d697de667876b419f770f8ec2e2743cc779c488
XimingFeng <xfeng36@asu.edu>
2016-10-23 01:55:19 -0700

    add a parenthesis

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -2,7 +2,7 @@
 	use Aws\S3\S3Client;
 	// require 'http://'.$_SERVER['HTTP_HOST'].'/s3/vendor/autoload.php';
 	echo 'http://'.$_SERVER['HTTP_HOST'].'/s3/vendor/autoload.php<br>';
-	echo 'If the file vender exits:   ' . file_exists('http://'.$_SERVER['HTTP_HOST'].'/s3/vendor/autoload.php')."<br>";
+	echo 'If the file vender exits:   ' . file_exists(('http://'.$_SERVER['HTTP_HOST'].'/s3/vendor/autoload.php'))."<br>";
 	date_default_timezone_set('UTC');
 	
 	try{
