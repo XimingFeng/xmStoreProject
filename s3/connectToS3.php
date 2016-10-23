c92c743473a3389f33d81d9f38b71c0b64e5a1d2
XimingFeng <xfeng36@asu.edu>
2016-10-23 02:50:18 -0700

    typo

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -2,7 +2,7 @@
 	use Aws\S3\S3Client;
 	// require 'http://'.$_SERVER['HTTP_HOST'].'/s3/vendor/autoload.php';
 	// echo "http://'$_SERVER['HTTP_HOST']'/s3/vendor/autoload.php<br>";
-	define("ROOT", "/var/www/html")
+	define("ROOT", "/var/www/html");
 	echo 'If the file s3 exits:   ' . file_exists(ROOT."/s3")."<br>";
 	echo $_SERVER['DOCUMENT_ROOT']."<br>";
 	// date_default_timezone_set('UTC');
