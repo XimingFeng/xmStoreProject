3e45c8cde32c6f099cc838c51df48871fcb669ee
XimingFeng <xfeng36@asu.edu>
2016-10-23 01:47:04 -0700

    change the require url with http_host

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -1,8 +1,8 @@
 <?php
 	use Aws\S3\S3Client;
-	require 'vendor/autoload.php';
+	require 'http://'.$_SERVER['HTTP_HOST'].'vendor/autoload.php';
 	
-	echo 'If the file vender exits:   ' . file_exists('vendor')."<br>";
+	echo 'If the file vender exits:   ' . file_exists('http://'.$_SERVER['HTTP_HOST'].'vendor/autoload.php')."<br>";
 	date_default_timezone_set('UTC');
 	
 	try{
