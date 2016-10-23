0792c77ff0a00e86bdbd812c4f080360e8a84e50
XimingFeng <xfeng36@asu.edu>
2016-10-23 02:14:51 -0700

    to see the url

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -1,7 +1,7 @@
 <?php
 	use Aws\S3\S3Client;
 	// require 'http://'.$_SERVER['HTTP_HOST'].'/s3/vendor/autoload.php';
-	echo 'http://'.$_SERVER['HTTP_HOST'].'/s3/vendor/autoload.php<br>';
+	echo "http://'$_SERVER['HTTP_HOST']'/s3/vendor/autoload.php<br>";
 	echo 'If the file vender exits:   ' . file_exists("http://".$_SERVER['HTTP_HOST']."/s3/vendor")."<br>";
 	date_default_timezone_set('UTC');
 	
