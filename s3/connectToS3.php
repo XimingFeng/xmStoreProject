7abc2dc3f98848f225d9a1fe53b48fbb2eb50166
XimingFeng <xfeng36@asu.edu>
2016-10-23 02:35:22 -0700

    sap

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -2,7 +2,7 @@
 	use Aws\S3\S3Client;
 	// require 'http://'.$_SERVER['HTTP_HOST'].'/s3/vendor/autoload.php';
 	// echo "http://'$_SERVER['HTTP_HOST']'/s3/vendor/autoload.php<br>";
-	echo 'If the file s3 exits:   ' . file_exists("/s3/")."<br>";
+	echo 'If the file s3 exits:   ' . file_exists("/var/www/http/s3")."<br>";
 	// date_default_timezone_set('UTC');
 	/*
 	try{
