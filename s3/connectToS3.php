7f184b1a1e77237635a45dd6cd2c4dfb74ad335f
XimingFeng <xfeng36@asu.edu>
2016-10-23 01:51:37 -0700

    miss s3 in url

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -1,8 +1,8 @@
 <?php
 	use Aws\S3\S3Client;
-	// require 'http://'.$_SERVER['HTTP_HOST'].'/vendor/autoload.php';
+	// require 'http://'.$_SERVER['HTTP_HOST'].'/s3/vendor/autoload.php';
 	
-	echo 'If the file vender exits:   ' . file_exists('http://'.$_SERVER['HTTP_HOST'].'/vendor/autoload.php')."<br>";
+	echo 'If the file vender exits:   ' . file_exists('http://'.$_SERVER['HTTP_HOST'].'/s3/vendor/autoload.php')."<br>";
 	date_default_timezone_set('UTC');
 	
 	try{
