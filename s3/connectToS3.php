c2fd2adf53113b7ae97f8c7d4545aed44cd8b6e9
XimingFeng <xfeng36@asu.edu>
2016-10-23 02:49:25 -0700

    define a global variable root as /var/www/heml

--- s3/connectToS3.php
+++ s3/connectToS3.php
@@ -2,7 +2,8 @@
 	use Aws\S3\S3Client;
 	// require 'http://'.$_SERVER['HTTP_HOST'].'/s3/vendor/autoload.php';
 	// echo "http://'$_SERVER['HTTP_HOST']'/s3/vendor/autoload.php<br>";
-	echo 'If the file s3 exits:   ' . file_exists("./s3")."<br>";
+	define("ROOT", "/var/www/html")
+	echo 'If the file s3 exits:   ' . file_exists(ROOT."/s3")."<br>";
 	echo $_SERVER['DOCUMENT_ROOT']."<br>";
 	// date_default_timezone_set('UTC');
 	/*
